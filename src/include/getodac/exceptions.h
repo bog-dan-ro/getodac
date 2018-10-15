087b885cafc6e1df03dd9517823e37d0a8f1251e
BogDan Vatra <bogdan@kde.org>
2018-10-15 14:22:09 +0300

    Add AGPL exception

--- src/include/getodac/exceptions.h
+++ src/include/getodac/exceptions.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2016, BogDan Vatra <bogdan@kde.org>
+    Copyright (C) 2018, BogDan Vatra <bogdan@kde.org>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as
@@ -13,6 +13,15 @@
 
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    AGPL EXCEPTION:
+    The AGPL license applies only to this file itself.
+
+    As a special exception, the copyright holders of this file give you permission
+    to use it, regardless of the license terms of your work, and to copy and distribute
+    them under terms of your choice.
+    If you do any changes to this file, these changes must be published under AGPL.
+
 */
 
 #ifndef EXCEPTIONS_H

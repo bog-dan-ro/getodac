bog-dan-ro/getodac

commit 2f50f658c865de1efe68c3ef625d067e6907af42

Various fixes

src/server/server_session.cpp
@@ -173,7 +173,7 @@ ServerSession *ServerSession::sessionReady()
 void ServerSession::processEvents(uint32_t events) noexcept
 {
     try {
-        if (events & (EPOLLERR | EPOLLRDHUP | EPOLLHUP) ) {
+        if (events & (EPOLLERR | EPOLLRDHUP | EPOLLHUP)) {
             terminateSession(Action::Quit);
             return;
         }
@@ -415,12 +415,13 @@ void ServerSession::readLoop(YieldType &yield)
     settings.on_body = &ServerSession::body;
     settings.on_message_complete = &ServerSession::messageComplete;
     std::vector<char> tempBuffer;
+    setTimeout();
     while (yield.get() == Action::Continue) {
         try {
             auto tempSize = tempBuffer.size();
             auto sz = sockRead(m_eventLoop->sharedReadBuffer.data() + tempSize, m_eventLoop->sharedReadBuffer.size() - tempSize);
 
-            if (sz <= 0) {
+            if (!sz) {
                 yield();
                 continue;
             }

src/server/sessions_event_loop.cpp
@@ -244,7 +244,7 @@ void SessionsEventLoop::loop()
 
         auto now = clock::now();
         auto duration = std::chrono::duration_cast<Ms>(now - before);
-        if ( duration < timeout && !wokeup) {
+        if (duration < timeout && !wokeup) {
             timeout -= duration;
         } else {
             std::unordered_set<ServerSession *> wokeupsessions;

conf/CMakeLists.txt
@@ -2,6 +2,8 @@ configure_file(server.conf ${CMAKE_BINARY_DIR}/etc/GETodac/server.conf)
 configure_file(server_logging.conf ${CMAKE_BINARY_DIR}/etc/GETodac/server_logging.conf)
 configure_file(server_ssl_ctx.conf ${CMAKE_BINARY_DIR}/etc/GETodac/server_ssl_ctx.conf)
 configure_file(staticFiles.conf ${CMAKE_BINARY_DIR}/etc/GETodac/staticFiles.conf)
+configure_file(server.crt ${CMAKE_BINARY_DIR}/etc/GETodac/server.crt COPYONLY)
+configure_file(server.key ${CMAKE_BINARY_DIR}/etc/GETodac/server.key COPYONLY)
 
 install(DIRECTORY ${CMAKE_BINARY_DIR}/etc/GETodac/ DESTINATION etc/GETodac
     FILES_MATCHING PATTERN "*.conf"

conf/server.conf
@@ -2,7 +2,7 @@
 
 queued_connections 40000 ; how many connection we keep in queue, default is 20000
 
-max_connections_per_ip 500 ; how many connections from a single ip we're accespting, if not set defaults to 500
+max_connections_per_ip 10000 ; how many connections from a single ip we're accepting, if not set defaults to 500
 
 workload_balancing true ; enable/disable the workload balancing. When enabled the
                         ; IPs with the least connections will be served first.
@@ -35,7 +35,7 @@ privileges {
 }
 
 https {
-    enabled false
+    enabled true
     port 8443 ; HTTPS Port
 
     ssl {

conf/server.crt
@@ -1,0 +1,21 @@
+-----BEGIN CERTIFICATE-----
+MIIDiTCCAnGgAwIBAgIUFbgjh5cDoVIhsYn6v3Mpp1ZHICkwDQYJKoZIhvcNAQEL
+BQAwVDELMAkGA1UEBhMCWFgxDjAMBgNVBAgMBVN0YXRlMREwDwYDVQQHDAhMb2Nh
+bGl0eTEMMAoGA1UECgwDT3JnMRQwEgYDVQQDDAtDb21tb24gTmFtZTAeFw0yMDA4
+MDEwOTIyMDZaFw0yMjA5MDgwOTIyMDZaMFQxCzAJBgNVBAYTAlhYMQ4wDAYDVQQI
+DAVTdGF0ZTERMA8GA1UEBwwITG9jYWxpdHkxDDAKBgNVBAoMA09yZzEUMBIGA1UE
+AwwLQ29tbW9uIE5hbWUwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDH
+3fsc74jsgb3bm9i7EFfPb/dBgK3hHczRXI4CHrSycsQUKGo/+b2IKgRZMBIDqsf4
+mj5tQsuhzf7k03/omSv54deccrFFss2eGF96AbQG4rG85YhVC3doepJMUSzT2C4B
+7FBV0teCAdRjkm4SJfXkb1GQt1sKT+Vny9Siqvao+AOUljxl0VFZHJHDvmDpbvrg
+etu6dMNKs0pReBtpny/3CQhISe5pD56kplH2Eqoqe6q5LWmsbBLApSE22RZolASe
+oZMqbfVfr8vsHzuHxQOyVkGOiVaFFPLIoXAKG75V38nuvBuLZYzAg1PYCc/IvyMn
+5kN8oc2tGve0Lrux6ZrFAgMBAAGjUzBRMB0GA1UdDgQWBBRK0GXRqixeN2dZtAJv
+57SOi9dlNjAfBgNVHSMEGDAWgBRK0GXRqixeN2dZtAJv57SOi9dlNjAPBgNVHRMB
+Af8EBTADAQH/MA0GCSqGSIb3DQEBCwUAA4IBAQAeeqObgVAM5TnQhTH/8LnPGTzi
+OlE/CVNl0Z1GNObAlrCR0tRajUtw7TvSyn8I8qRSVqtT3e+mPiEfo6qbvpD5qOly
+F6Exit8+zyQl2xMLl2+wUhHeACcExrgcBUPfRsJ90BA+xGRNFnMmghDn8UF93GoK
+x18HHNjo4ebH2QW+BCArKL4/s5L8lcD4ryJFySOmzxiazYuZfASyPZSImUOAlXLI
+aYYM6f/oVebkk9ENHe63tOWMF7/x6IIKGgQ38DY1FO54PWQKPInVtCxdJFYI7b74
+KycEfC0WxA2SYYoQleC4qzDKvJCcsNXxGKKHDnY2fNxLY9mO7cYjDMa0Ufod
+-----END CERTIFICATE-----

conf/server.key
@@ -1,0 +1,27 @@
+-----BEGIN RSA PRIVATE KEY-----
+MIIEowIBAAKCAQEAx937HO+I7IG925vYuxBXz2/3QYCt4R3M0VyOAh60snLEFChq
+P/m9iCoEWTASA6rH+Jo+bULLoc3+5NN/6Jkr+eHXnHKxRbLNnhhfegG0BuKxvOWI
+VQt3aHqSTFEs09guAexQVdLXggHUY5JuEiX15G9RkLdbCk/lZ8vUoqr2qPgDlJY8
+ZdFRWRyRw75g6W764HrbunTDSrNKUXgbaZ8v9wkISEnuaQ+epKZR9hKqKnuquS1p
+rGwSwKUhNtkWaJQEnqGTKm31X6/L7B87h8UDslZBjolWhRTyyKFwChu+Vd/J7rwb
+i2WMwINT2AnPyL8jJ+ZDfKHNrRr3tC67semaxQIDAQABAoIBAQCFXA+fKhFRxew5
+6Y4+m1QMHKh9s2/SAsFUfQ5gSb7GRx+gV6s1jt0xSt/l5tZOThjqP3kiDGy1RS8r
+3he9yt51+ISWZkz0ta7PK3IhhZtq8L4pzXofTOxa8VZGx+9CVz+zlsuvyGR14eSm
+n+EECQI+byychEuh4NewX2g9Nhgpe0zxNw9gSsKzTdJ31RtSpxZxpBHk7kUmIq8g
+b0a9IaE67CQZLclBdgArJs8GepjU30HDgVvnpo42mJheetj30rNAxEU6N/icIT4q
+eleOCurquXiD9ysanszJ379Q16DjrYuS7PtUyLWXy+9mVovFMUNsFiF7BbPD/g19
+Alw7X50hAoGBAPCKqgKJlXtiMqsUrYDhNnJznYem91AOpcqU8N7sVfHSf5wiSDgj
+PFqp4G+eV5adqhwG55lhmfj/NTNjZPAyRy1e9oc+WQw8C6x8hOKK/DYw5G0Riu+1
+K65AHmZ6DwAUO5NC5qCeirnBaVeF5XXFavp5O3ZRDHx/p7DeBlLUA8oNAoGBANS2
+Jgn89UfSvj3VAn2HtMhfaJR/BQ0oXyt/xUdLP23OAM1NtHf4csK2FFj0KIIBTkK7
+sQOZSElqAVq24j3P/lFFj3Ah1wnLJ7fvROkg1P9TyoC2KAlbOB81Ymebir7bi/47
+zc9DQ1rzzjsKpP305dmJa1aS/dEGMV3rYV62E/2ZAoGAEvBYAmsiwdwxcRbnskYJ
+K2m+9XdH8Z21oAWqEWM6QRf5xClO1lyv29OUQDWPX+R6CEri4Ui1/szZO3qP7/mF
+cSUQ9AUMrdHseWA0dkHLKdkfoxbGgakaD1YtDpR9qdvt9OJHlk3lTkrUKHf6YRNy
+INZ6wA4RlZvXcC2LAep/geECgYBtgJ+wgKKcqOb62c+AY/4zMLqbx5AIKAJmWE5s
+11X95xEOn7QKFsKtbiKncHnyA04QmKMV4TEPahzsYD2z6IFZwV+IMEONbSIzVF+7
+TAWas74J6LxPpMANgOP5xl70xf0ZkhvvWQcxqVU/m8k4W3yAMngph15W3sNHQEdO
+BJnh8QKBgEL+VcFZmo0Hz3LB5HMWrBQtOxidn6VhH/xAbQ03VJhJG2lpzB1aTy65
+H3+jL8fgcyGWHYIx/427J7OvFeHIgsblOEiFajAVcGywod4yCpf5ecRal2mtN4Ks
+t+HGvB0WCglUVZQV0IfGD7hcC/CCS7xM8Ngcz8tB/F18cJ2PGAbj
+-----END RSA PRIVATE KEY-----

conf/server_ssl_ctx.conf
@@ -14,7 +14,7 @@ Certificate server.crt
 PrivateKey server.key
 
 ; Attempts to load the file *chain.pem* as the certificate chain for the server context.
-ChainCAFile chain.pem
+; ChainCAFile chain.pem
 
 ; Set minimum supported protocol
 MinProtocol TLSv1.2

src/server/secured_server_session.cpp
@@ -28,7 +28,6 @@ SecuredServerSession::SecuredServerSession(SessionsEventLoop *eventLoop, int soc
 
     if (!SSL_set_fd(m_SSL, sock))
         throw std::runtime_error(ERR_error_string(SSL_get_error(m_SSL, 0), nullptr));
-
     SSL_set_accept_state(m_SSL);
 }
 

commit 8800d06a45a6736ff01dd8014c54e2b0c3ee552e

Fix mistake introduced in 2f50f658c8

src/server/server_session.cpp
@@ -417,8 +417,7 @@ AbstractServerSession::Action ServerSession::processRequest(YieldType &yield)
         try {
             auto tempSize = tempBuffer.size();
             auto sz = sockRead(m_eventLoop->sharedReadBuffer.data() + tempSize, m_eventLoop->sharedReadBuffer.size() - tempSize);
-
-            if (!sz) {
+            if (sz <= 0) {
                 yield();
                 continue;
             }
@@ -431,6 +430,7 @@ AbstractServerSession::Action ServerSession::processRequest(YieldType &yield)
                 wakeuppper().wakeUp();
                 return Action::Quit;
             }
+
             if (!m_processRequest)
                 return Action::Continue;
             tempBuffer.clear();

src/server/sessions_event_loop.cpp
@@ -56,7 +56,7 @@ SessionsEventLoop::SessionsEventLoop()
 {
     unsigned long rmem_max = readProc("/proc/sys/net/core/rmem_max");
 #ifdef ENABLE_STRESS_TEST
-    rmem_max = 8; // super small bufer needed to test the partial parsing
+    rmem_max = 13; // super small bufer needed to test the partial parsing
 #endif
 
     // This buffer is shared by all ServerSessions which are server

tests/server_tests/Responses.cpp
@@ -138,9 +138,8 @@ void testPostPPP(const std::string &url, const std::string &data, const std::str
     curl.ingnoreInvalidSslCertificate();
     auto reply = curl.request("PATCH", data);
     EXPECT_EQ(reply.status, status);
-    for (uint32_t i = 0 ; i < reply.body.size(); ++i) {
-        EXPECT_EQ(hugeData[i], reply.body[i]);
-    }
+    EXPECT_EQ(reply.body.size(), data.size());
+    EXPECT_EQ(hugeData, reply.body);
 }
 
 TEST_P(Responses, testPPP)

tests/server_tests/StressServer.cpp
@@ -23,8 +23,6 @@
 namespace {
 using namespace std;
 
-using Stress = testing::TestWithParam<std::string>;
-
 void parseEchoData(const std::string &data, size_t &contentLength, std::unordered_map<string, string> &headers, std::string &body)
 {
     contentLength = 0;
@@ -48,6 +46,7 @@ void parseEchoData(const std::string &data, size_t &contentLength, std::unordere
         pos = p + 1;
     }
 }
+
 const std::string testBodyData = R"(/*
      Copyright (C) 2020, BogDan Vatra <bogdan@kde.org>
 
@@ -64,12 +63,11 @@ const std::string testBodyData = R"(/*
      You should have received a copy of the GNU Affero General Public License
      along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */)";
-TEST_P(Stress, server)
+TEST(Stress, server)
 {
     try {
         Getodac::Test::EasyCurl curl;
-        EXPECT_NO_THROW(curl.setUrl(url(GetParam(), "/echoTest")));
-        curl.ingnoreInvalidSslCertificate();
+        EXPECT_NO_THROW(curl.setUrl("http://localhost:8080/echoTest"));
         EXPECT_NO_THROW(curl.setHeaders({
                                             {"Super__________________long_______________field",
                                              "with___________super________log____---------value"}
@@ -91,7 +89,5 @@ TEST_P(Stress, server)
         EXPECT_NO_THROW(throw);
     }
 }
-
-INSTANTIATE_TEST_CASE_P(Stress, Stress, testing::Values("http", "https"));
 
 } // namespace
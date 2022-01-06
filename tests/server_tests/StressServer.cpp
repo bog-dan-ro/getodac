72963f67ceefd8084cb108fb3c1644c7f65d9c89
BogDan Vatra <bogdan@kde.org>
2022-01-06 17:11:50 +0200

    Happy new year

--- tests/server_tests/StressServer.cpp
+++ tests/server_tests/StressServer.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2021, BogDan Vatra <bogdan@kde.org>
+    Copyright (C) 2022, BogDan Vatra <bogdan@kde.org>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as
@@ -48,7 +48,7 @@ void parseEchoData(const std::string &data, size_t &contentLength, std::unordere
 }
 
 const std::string testBodyData = R"(/*
-     Copyright (C) 2021, BogDan Vatra <bogdan@kde.org>
+     Copyright (C) 2022, BogDan Vatra <bogdan@kde.org>
 
      This program is free software: you can redistribute it and/or modify
      it under the terms of the GNU Affero General Public License as
